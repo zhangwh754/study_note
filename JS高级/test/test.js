f172288ffed8c6bd8c73ce84e46fc1bdbf900556
张文皓 <zhangwh754@163.com>
2022-10-01 23:20:18 +0800

    ✨ feat: upload

--- JS高级/test/test.js
+++ JS高级/test/test.js
@@ -1,34 +1,15 @@
-const deepClone = obj => {
-  if (obj === null) return null
-  let clone = Object.assign({}, obj)
-  Object.keys(clone).forEach(
-    key => (clone[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
-  )
-  if (Array.isArray(obj)) {
-    clone.length = obj.length
-    return Array.from(clone)
-  }
-  return clone
+function Person(name) {
+  this.name = name
 }
 
-Function.prototype.myApply = function (thisArg, args) {
-  const fn = this
-  const _this = thisArg ? deepClone(Object(thisArg)) : window
-  _this.fn = fn
-  _this.fn(...args)
-  console.log(_this)
-}
-// Function.prototype.myCall = () => console.log('myCall')
-// Function.prototype.myBind = () => console.log('myBind')
-
-const add = function (num1, num2) {
-  console.log(this)
-  // console.log(num1 + num2)
-  // console.log(this.myName)
+Person.prototype.getName = function () {
+  console.log(this.name)
 }
 
-// foo.apply()
-const obj = { myName: 'zwh', fn: 2 }
-// add.myApply(obj, [1, 2])
-add.myApply('abc', [1, 2])
-// add.myApply(null, [1, 2])
+const p1 = new Person('zwh')
+const p2 = new Person('li')
+
+p1.getName()
+p2.getName()
+
+console.log(p1.getName === p2.getName)
