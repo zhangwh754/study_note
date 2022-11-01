81bedf0f5bfa37b8c1d5c96d9f086181658c3c83
张文皓 <zhangwh754@163.com>
2022-11-01 22:52:17 +0800

    ✨ feat: 文件上传

--- Node/index.js
+++ Node/index.js
@@ -1,14 +1,42 @@
 const http = require('http')
-const url = require('url')
+const qs = require('querystring')
+const fs = require('fs')
 
 const port = 8000
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-type': 'text/html;charset=utf8'
-  })
-  // res.setHeader('Content-type', 'text/html;charset=utf8')
-  res.end('<h2 style="color: red">Hello World</h2>')
+  if (req.url === '/upload') {
+    if (req.method === 'POST') {
+      // 必须设置，使用二级制处理文件
+      req.setEncoding('binary')
+      let body = ''
+      // 获取boundary
+      const boundary = req.headers['content-type'].split(';')[1].split('=')[1]
+      req.on('data', data => {
+        body += data
+      })
+      req.on('end', () => {
+        // 1.获取image/png的位置
+        const payload = qs.parse(body, '\r\n', ': ')
+        const type = payload['Content-Type']
+
+        // 2.开始在image/png的位置进行截取
+        const typeIndex = body.indexOf(type)
+        const typeLength = type.length
+        let imageData = body.substring(typeIndex + typeLength)
+
+        // 3.将中间的两个空格去掉
+        imageData = imageData.replace(/^\s\s*/, '')
+
+        // 4.将最后的boundary去掉
+        imageData = imageData.substring(0, imageData.indexOf(`--${boundary}--`))
+
+        fs.writeFile('./foo.png', imageData, 'binary', err => {
+          res.end('文件上传成功~')
+        })
+      })
+    }
+  }
 })
 
 server.listen(port, '0.0.0.0', () => {
