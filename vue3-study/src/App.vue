c731864bcb29ecceb8ccc7cef7af9144f745f7a5
张文皓 <zhangwh754@163.com>
2022-11-16 14:57:12 +0800

    🎉 init: 初始化

---
+++ vue3-study/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView, RouterLink, useRoute } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/" v-if="useRoute().path === '/about'">
+      <button>go home</button>
+    </RouterLink>
+    <RouterLink to="/about" v-else>
+      <button>go about</button>
+    </RouterLink>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ vue3-study/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+.about {
+  color: blue;
+}
+</style>
