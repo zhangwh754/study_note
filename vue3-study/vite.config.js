c7af1716cd3a94c31db6fe928c711d9cd40d8a09
张文皓 <zhangwh754@163.com>
2022-11-16 19:08:15 +0800

    ✨ feat: 入口文件动态引入

--- vue3-study/vite.config.js
+++ vue3-study/vite.config.js
@@ -2,13 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      createHtmlPlugin({
+        minify: true,
+        entry: mode === 'production' ? 'src/main.prod.js' : 'src/main.dev.js'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      open: true
     }
   }
 })
